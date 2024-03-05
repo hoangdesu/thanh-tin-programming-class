90352d0965dbf98e12d41c349ad0da6e86468738
hoangdesu <hoangdesu@gmail.com>
2024-03-05 16:22:40 +0700

    ArrayList

---
+++ HelloJava/src/Mar5_Methods.java
@@ -0,0 +1,22 @@
+//public class Mar5_Methods {
+//    public static void main(String[] args) {
+//        Enter a year: 2024
+//
+//        if (isLeapYear(year)) {
+//            > 2024 is a leap year
+//        } else {
+//            > 2023 is not a leap year
+//        }
+//    }
+//}
+//
+////methods: function
+////- input and/or output
+//
+//isLeapYear(year (so nguyen)) -> boolean
+//-> nam nhuan: 366 ngay
+
+
+
+
+//Dynamic array: array dong
