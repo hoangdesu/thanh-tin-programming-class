567ddcf55d77bc7ac56a316dd5ff16a326a18a5c
hoangdesu <hoangdesu@gmail.com>
2024-03-12 12:02:11 +0700

    than so hoc

---
+++ HelloJava/src/Mar7_ThanSoHoc.java
@@ -0,0 +1,24 @@
+public class Mar7_ThanSoHoc {
+    public static void main(String[] args) {
+//        thanSoHoc("26/02/1995"); => 7
+//        thanSoHoc("01/11/2004"); => 9
+//        thanSoHoc("02/26/1995"); => 7
+
+    }
+
+    public static int thanSoHoc(String dob) {
+//        return thansohoc: int;
+        return 0;
+    }
+}
+
+//1/11/2004
+//1+1+1+2+4=9
+
+//nen dung arraylist (optional)
+
+
+//26/2/1995
+//2+6+2+1+9+9+5=34
+//3+4=7
+//=> than so hoc cua ban la 7
