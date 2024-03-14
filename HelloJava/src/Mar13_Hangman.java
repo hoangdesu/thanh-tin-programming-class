a7b5c410e094b274c8a858788dfcd8e08dd3280b
hoangdesu <hoangdesu@gmail.com>
2024-03-14 15:59:41 +0700

    shuffle ArrayList + than so hoc

---
+++ HelloJava/src/Mar13_Hangman.java
@@ -0,0 +1,17 @@
+public class Mar13_Hangman {
+    public static void main(String[] args) {
+        String description = "Capital city of Vietnam";
+        String keyword = "hanoi";
+
+//        System.out.println(description);
+//
+//        _ _ _ _ _
+//
+//        doan 1 chu: a
+//
+//        _ a _ _ _
+
+
+//        String X -> String[] or ArrayList<String>
+    }
+}
