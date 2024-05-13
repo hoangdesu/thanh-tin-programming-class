76fd9c80cfd5f61c82a9fbaa8763107934848c3e
hoangdesu <hoangdesu@gmail.com>
2024-05-14 03:43:24 +0700

    module 3: web development with java, basic HTML elements

---
+++ ProgrammingStudio1/Module3-Web/demo-web/script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('#btn-order');
+
+button.addEventListener('click', () => {
+    alert('Your food has been order!!! :D')
+});
---
+++ ProgrammingStudio1/Module3-Web/exercises/May11_String.java
@@ -0,0 +1,24 @@
+package exercises;
+
+public class May11_String {
+    public static void main(String[] args) {
+        // parseNumberToVietnamese(16) -> "mười sáu"
+
+        // 16 = 10 - 6
+
+        // 28 = hai + mươi + tám
+        // 87 = tám + mươi + bảy
+
+        // 55 = năm + mươi + lăm
+
+        // limit: 0 - 999
+
+        // hàng đơn vị
+        // hàng chục
+        // hàng trăm
+
+        // 987 = 9 + trăm + 8 + mươi + 7
+    }
+
+    // parseNumberToVietnamese(int num) -> String
+}
