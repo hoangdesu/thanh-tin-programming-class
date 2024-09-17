d10a2717900bf6e50a19c19246b95ab85668eae7
hoangdesu <hoangdesu@gmail.com>
2024-09-17 16:39:46 +0700

    mongo, template ejs, expressjs, final group project for web studio

---
+++ WebStudio/javascript/objects.js
@@ -0,0 +1,19 @@
+// const 
+
+// Data structures
+
+// let name = 'Tin';
+// let age = 20;
+
+// object: collections of key and value pairs
+
+const student1 = {
+    name: 'Tin',
+    age: 20,
+    // ...
+}
+
+console.log(student1.name);
+
+// arrays
+const exes = ['hân', 'linh', 'bảo hân', 'kim', 'tú anh']
