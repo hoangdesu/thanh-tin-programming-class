c4446cef357b84104b39f24db564d29becdef05c
hoangdesu <hoangdesu@gmail.com>
2024-10-01 17:03:15 +0700

    finished part 1, part 2 question 1

--- WebStudio/Final-Exam-Tutorial/materials/app.js
+++ WebStudio/Final-Exam-Tutorial/materials/app.js
@@ -1,19 +1,27 @@
 // To enable the .env
 require('dotenv').config();
 
+
 // App setup
 const express = require('express');
 const app = express();
+
+
+// su dung engine ejs
+app.set('view engine', 'ejs');
+
 
 // Home endpoint
 app.get('/', function (req, res) {
   res.render('index');
 });
 
+
 // Meal endpoint
 app.get('/meal/:name', function (req, res) {
   res.render('meal');
 });
+
 
 // Start the server
 const port = 3000;
--- WebStudio/Final-Exam-Tutorial/materials/db/mongoose.js
+++ WebStudio/Final-Exam-Tutorial/materials/db/mongoose.js
@@ -7,7 +7,9 @@ const mongoose = require('mongoose');
 // Change <sid> to your real student id, e.g., 2024b_final_v1234567
 // const mongoUrl = "mongodb+srv://<username>:<password>@<host>/2024b_final_<sid>?retryWrites=true&w=majority&appName=Cluster0";
 
-const mongoUrl = "mongodb+srv://s3970879:Tkr9GRM44uNyJ8eA@cluster0.jnhpi.mongodb.net/2024b_final_tin_tutorial?retryWrites=true&w=majority&appName=Cluster0";
+const mongoUrl = "mongodb+srv://s3970879:Tkr9GRM44uNyJ8eA@cluster0.jnhpi.mongodb.net/2024b_final_tin_tutorial?retryWrites=true&w=majority&appName=Cluster0"; // tutorial db
+
+// const mongoUrl = "mongodb+srv://s3970879:Tkr9GRM44uNyJ8eA@cluster0.jnhpi.mongodb.net/2024b_final_s3970879_tutorial?retryWrites=true&w=majority&appName=Cluster0"; // tin's db
 
 // "mongodb+srv://s3970879:Tkr9GRM44uNyJ8eA@cluster0.jnhpi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 //  "mongodb+srv://s3970879:Tkr9GRM44uNyJ8eA@cluster0.jnhpi.mongodb.net/2024b_final_s3970879?retryWrites=true&w=majority&appName=Cluster0";
