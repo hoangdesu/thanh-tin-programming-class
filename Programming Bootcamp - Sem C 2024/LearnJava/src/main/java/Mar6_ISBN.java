50fdfc15e76af712c33ce6dc08949c6a5e0a493c
hoangdesu <hoangdesu@gmail.com>
2025-03-06 20:08:33 +0700

    isbn function

---
+++ Programming Bootcamp - Sem C 2024/LearnJava/src/main/java/Mar6_ISBN.java
@@ -0,0 +1,21 @@
+public class Mar6_ISBN {
+    public static void main(String[] args) {
+//        isISBNValid()
+//        cout << endl << "valid?: " << is_isbn_valid("9780-306-40615-7") << endl; // true->1
+//        cout << endl << "valid?: " << is_isbn_valid("9780306406157") << endl; // true->1
+//        cout << endl << "valid?: " << is_isbn_valid("978030640615-7") << endl; // true->1
+//        cout << endl << "valid?: " << is_isbn_valid("9780-306-40615-8") << endl; // false->0
+
+
+    }
+
+    public static boolean isISBNValid(String isbn) {
+        return false;
+    }
+
+    boolean liked = false;
+
+    liked -> emotes: ['haha', 'huhu', 'like', 'tim', 'angry']
+
+    HOMEWORK: extend the same function to support ISBN10
+}
