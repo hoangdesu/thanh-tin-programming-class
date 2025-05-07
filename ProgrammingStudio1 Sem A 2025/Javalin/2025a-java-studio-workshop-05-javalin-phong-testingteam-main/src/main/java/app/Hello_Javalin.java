eaedbfa16949c0c6ec6372396a12fa1bd6760987
hoangdesu <hoangdesu@gmail.com>
2025-05-07 09:07:20 +0700

    added jackson to return arraylist

--- ProgrammingStudio1 Sem A 2025/Javalin/2025a-java-studio-workshop-05-javalin-phong-testingteam-main/src/main/java/app/Hello_Javalin.java
+++ ProgrammingStudio1 Sem A 2025/Javalin/2025a-java-studio-workshop-05-javalin-phong-testingteam-main/src/main/java/app/Hello_Javalin.java
@@ -1,19 +1,27 @@
 package app;
 
+import java.util.ArrayList;
+
 import io.javalin.Javalin;
 import io.javalin.core.util.RouteOverviewPlugin;
 
-
-public class May5_Hello_Javalin {
+public class Hello_Javalin {
 
     public static final int         JAVALIN_PORT    = 7001;
 
     public static void main(String[] args) {
 
+        ArrayList<String> drinks = new ArrayList<>();
+
+        drinks.add("Ca phe sua da");
+        drinks.add("Matcha latte");
+        drinks.add("Bac xiu");
+
         Javalin app = Javalin.create()
             .get("/", ctx -> ctx.result("Hello World"))
             .get("/hi", ctx -> ctx.result("Hiiiii"))
             .get("/tin", ctx -> ctx.result("Hello Tin :D"))
+            .get("/drinks", ctx -> ctx.json(drinks))
             .start(JAVALIN_PORT);
     }
 }
