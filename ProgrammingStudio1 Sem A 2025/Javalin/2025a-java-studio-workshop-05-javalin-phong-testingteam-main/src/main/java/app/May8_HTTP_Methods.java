097bcd2819040521cc368737aec0b1c0b1cf16e8
hoangdesu <hoangdesu@gmail.com>
2025-05-09 00:15:38 +0700

    HTTP protocols, Request methods, Postman

---
+++ ProgrammingStudio1 Sem A 2025/Javalin/2025a-java-studio-workshop-05-javalin-phong-testingteam-main/src/main/java/app/May8_HTTP_Methods.java
@@ -0,0 +1,45 @@
+package app;
+
+import io.javalin.Javalin;
+import io.javalin.core.util.RouteOverviewPlugin;
+
+
+/**
+ * Main Application Class.
+ * <p>
+ * Running this class as regular java application will start the 
+ * Javalin HTTP Server and our web application.
+ *
+ * @author Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
+ * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
+ */
+public class May8_HTTP_Methods {
+
+    public static final int         JAVALIN_PORT    = 7001;
+    public static final String      CSS_DIR         = "css/";
+    public static final String      IMAGES_DIR      = "images/";
+
+    public static void main(String[] args) {
+        // Create our HTTP server and listen in port 7001
+        Javalin app = Javalin.create(config -> {
+            config.registerPlugin(new RouteOverviewPlugin("/help/routes"));
+            
+            // Uncomment this if you have files in the CSS Directory
+            config.addStaticFiles(CSS_DIR);
+
+            // Uncomment this if you have files in the Images Directory
+            config.addStaticFiles(IMAGES_DIR);
+        }).start(JAVALIN_PORT);
+
+
+        // Configure Web Routes
+        configureRoutes(app);
+    }
+
+    public static void configureRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("GET request from client"));
+        app.post("/", ctx -> ctx.result("POST request for Homepage"));
+        app.put("/", ctx -> ctx.result("Edited data from database"));
+        
+    }
+}
